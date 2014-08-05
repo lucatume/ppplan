6689afb797cb312ba0303167a4acc148ee4ff6fc
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-08-05 11:42:46 +0200

    implemented ListFormatter methods to format lines, head and foot of the list

--- src/ListFormatter.php
+++ src/ListFormatter.php
@@ -5,11 +5,37 @@ namespace PPPlan;
 
 class ListFormatter
 {
+    protected $format = 'txt';
+    protected $legitFormats = array(
+        'txt',
+        'taskpaper'
+    );
+    
+    public function __construct($format = null)
+    {
+        $this->format = ($format and in_array($format, $this->legitFormats)) ? $format : 'txt';
+    }
+    public function formatLine(Task $task)
+    {
+        $out = '';
+        $indent = "\t";
+        switch ($this->format) {
+            case 'taskpaper':
+                $out = sprintf('%s- %s @est(%s)', $indent, $task->title, $task->hours);
+                break;
+
+            default:
+                $out = sprintf('%s- %s (est. %s hr%s)', $indent, $task->title, $task->hours, Utils::getPluralSuffixFor($task->hours));
+                break;
+        }
+        return $out;
+    }
     public function createListFrom(Objective $objective, array $tasks, $format = 'txt')
     {
         $fileList = sprintf('Things to do to %s', $objective->title);
-        $fileList .= "\n";
+        $fileList.= "\n";
         $objective->totalHours = 0;
+        
         // remove the objective from the tasks
         unset($tasks[0]);
         $tasks = array_values($tasks);
@@ -18,11 +44,41 @@ class ListFormatter
                 continue;
             }
             $tabs = "\n\t";
-            $fileList .= sprintf('%s- %s (est. %s hr%s)', $tabs, $task->title, $task->hours, Utils::getPluralSuffixFor($task->hours));
-            $objective->totalHours += $task->hours;
+            $fileList.= sprintf('%s- %s (est. %s hr%s)', $tabs, $task->title, $task->hours, Utils::getPluralSuffixFor($task->hours));
+            $objective->totalHours+= $task->hours;
         }
-        $fileList .= "\n\n";
-        $fileList .= sprintf('that\'s a total estimate of %s hour%s', $objective->totalHours, Utils::getPluralSuffixFor($objective->totalHours));
+        $fileList.= "\n\n";
+        $fileList.= sprintf('that\'s a total estimate of %s hour%s', $objective->totalHours, Utils::getPluralSuffixFor($objective->totalHours));
         return $fileList;
+    }
+    public function formatHead(Objective $objective)
+    {
+        $out = '';
+        $newlinesAfterHead = "\n\n";
+        switch ($this->format) {
+            case 'taskpaper':
+                $out = sprintf('%s: @est(%s)%s', ucfirst($objective->title) , $objective->totalHours, $newlinesAfterHead);
+                break;
+
+            default:
+                $out = sprintf('Things to do to %s:%s', lcfirst($objective->title) , $newlinesAfterHead);
+                break;
+        }
+        return $out;
+    }
+    public function formatFoot(Objective $objective)
+    {
+        $out = '';
+        $beforeHead = "\n";
+        switch ($this->format) {
+            case 'taskpaper':
+                $out = '';
+                break;
+
+            default:
+                $out = sprintf('%sThat\'s a total estimate of %s hour%s.', $beforeHead, $objective->totalHours, Utils::getPluralSuffixFor($objective->totalHours));
+                break;
+        }
+        return $out;
     }
 }
--- tests/unit/ListFormatterTest.php
+++ tests/unit/ListFormatterTest.php
@@ -1,19 +1,18 @@
 <?php
-
 use PPPlan\ListFormatter;
+use PPPlan\Task;
+use PPPlan\Objective;
 
 class ListFormatterTest extends \PHPUnit_Framework_TestCase
 {
-    protected $sut;
     protected function setUp()
     {
-        $this->sut = new ListFormatter();
     }
-
+    
     protected function tearDown()
     {
     }
-
+    
     /**
      * @test
      * it should be instantiatable
@@ -22,13 +21,95 @@ class ListFormatterTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf('\PPPLan\ListFormatter', new ListFormatter());
     }
-
+    
     /**
      * @test
-     * it should format a line to a format
+     * it should format a line to taskpaper format
      */
-    public function it_should_format_a_line_to_a_format()
+    public function it_should_format_a_line_to_taskpaper_format()
     {
-        $task = new \PPPlan\Task();
+        $task = new Task('do some task', 4, false);
+        $sut = new ListFormatter('taskpaper');
+        $line = $sut->formatLine($task);
+        $this->assertEquals("\t- do some task @est(4)", $line);
     }
+    
+    /**
+     * @test
+     * it should format a line to plain text
+     */
+    public function it_should_format_a_line_to_plain_text()
+    {
+        $task = new Task('do some task', 4, false);
+        $sut = new ListFormatter('txt');
+        $line = $sut->formatLine($task);
+        $this->assertEquals("\t- do some task (est. 4 hrs)", $line);
+    }
+    
+    /**
+     * @test
+     * it should use singular when estimate is an hour or less in plain text format
+     */
+    public function it_should_use_singular_when_estimate_is_an_hour_or_less_in_plain_text_format()
+    {
+        $task = new Task('do some task', 1, false);
+        $sut = new ListFormatter('txt');
+        $line = $sut->formatLine($task);
+        $this->assertEquals("\t- do some task (est. 1 hr)", $line);
+    }
+        /**
+         * @test
+         * it should format the head of the list to taskpaper format
+         */
+        public function it_should_format_the_head_of_the_list_to_taskpaper_format()
+        {
+            $objective = new Objective('do something', 5);
+            $sut = new ListFormatter('taskpaper');
+            $line = $sut->formatHead($objective);
+            $this->assertEquals("Do something: @est(5)\n\n", $line);
+        }
+        /**
+         * @test
+         * it should format the head of the list to plain text format
+         */
+        public function it_should_format_the_head_of_the_list_to_plain_text_format()
+        {
+        $objective = new Objective('do something', 5);
+        $sut = new ListFormatter('txt');
+        $line = $sut->formatHead($objective);
+        $this->assertEquals("Things to do to do something:\n\n", $line);
+        }
+        /**
+         * @test
+         * it should output floating hours when formatting lines
+         */
+        public function it_should_output_floating_hours_when_formatting_lines()
+        {
+            $task = new Task('do some task', 1.3, false);
+            $sut = new ListFormatter('txt');
+            $line = $sut->formatLine($task);
+            $this->assertEquals("\t- do some task (est. 1.3 hrs)", $line);
+        }
+        /**
+         * @test
+         * it should format the foot of the list to taskpaper format
+         */
+        public function it_should_format_the_foot_of_the_list_to_taskpaper_format()
+        {
+        $objective = new Objective('do something', 5);
+        $sut = new ListFormatter('taskpaper');
+        $line = $sut->formatFoot($objective);
+        $this->assertEquals("", $line);
+        }
+        /**
+         * @test
+         * it should format the foot of the list to plain text format
+         */
+        public function it_should_format_the_foot_of_the_list_to_plain_text_format()
+        {
+        $objective = new Objective('do something', 5);
+        $sut = new ListFormatter('txt');
+        $line = $sut->formatFoot($objective);
+        $this->assertEquals("\nThat's a total estimate of 5 hours.", $line);
+        }
 }
