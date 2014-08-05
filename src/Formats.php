db2761489dd9fcb2a1d4c2d6e729e7d741301211
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-08-05 15:25:18 +0200

    added the Formats class to deal with anything format related

---
+++ src/Formats.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PPPlan;
+
+
+class Formats
+{
+    protected static $validFormats = array('txt', 'taskpaper');
+
+    public static function getValidFormatFor($format)
+    {
+        return (isset($format) and in_array($format, self::$validFormats)) ? $format : self::$validFormats[0];
+    }
+} 
